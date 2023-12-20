3ef680d3dcdb1c0f754d9e9c89a044b35017ad26
Kayque Orlando <devkayque@hotmail.com>
2023-12-20 15:59:13 -0300

    1.0

--- src/br/com/cod3er/cm/modelo/CampoEvent.java
+++
@@ -1,6 +0,0 @@
-package br.com.cod3er.cm.modelo;
-
-public enum CampoEvent {
-
-	ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
-}
--- src/br/com/cod3er/cm/modelo/CampoObserver.java
+++
@@ -1,6 +0,0 @@
-package br.com.cod3er.cm.modelo;
-
-public interface CampoObserver {
-
-	public void eventoOcorreu( Campo campo, CampoEvent evento);
-}
